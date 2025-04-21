b46ae0cd219a52ed94f3887a3386297c57482848
EstuViani <eviani94@hotmail.com>
2025-04-21 16:21:41 -0300

    Add specific

---
+++ backend/app/bd/cruds/crud_specific.py
@@ -0,0 +1,46 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select, update
+from app.models.SpecificSchedule import SpecificSchedule
+from app.bd.schemas import schema_specific
+#Aqui se crearan las funciones que utilizaran los esquemas y modelos
+from datetime import date, time
+
+from app.bd.bd_utils import strip_time_hour_minute
+
+
+def get_all_specific(db: Session):
+    return db.query(SpecificSchedule).all()
+
+
+def create_specific(db: Session, spec: schema_specific.SpecificCreate, id_prof:int):
+    spec.start = strip_time_hour_minute(spec.start) #10:20:06.25..z -> 10:20
+    spec.end= strip_time_hour_minute(spec.end)
+
+    try:
+        db_spec = SpecificSchedule(**spec.dict(), user_id=id_prof)
+        db.add(db_spec)
+        db.commit()
+        db.refresh(db_spec)
+    except:
+        db_spec = {'Error':'on create_spec'}
+    return db_spec
+
+def iscaceled_specific(db:Session, day_in:date, id_prof:int, hour:time) -> schema_specific.SpecificIsCancel:
+    hour = strip_time_hour_minute(hour)
+    smt = select(SpecificSchedule).where( SpecificSchedule.day == day_in, 
+                                             SpecificSchedule.user_id == id_prof,
+                                             SpecificSchedule.start == hour)
+    response = db.scalars(smt).all()
+    return response
+
+def cancel_day(db:Session, prof_id:int, day:date, hour:time):
+    hour = strip_time_hour_minute(hour)
+    db.query(SpecificSchedule).filter(SpecificSchedule.day == day, 
+                                      SpecificSchedule.start == hour, 
+                                      SpecificSchedule.user_id == prof_id).update({"isCanceling":True})
+    db.commit()
+    return db.query(SpecificSchedule).all()
+
+def get_day(db:Session, prof_id:int):
+    return  db.query(SpecificSchedule).filter(SpecificSchedule.user_id == prof_id).all()
+
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,4 @@
 from .Profesional import Profesional
-#from .SpecificSchedule import SpecificSchedule
+from .SpecificSchedule import SpecificSchedule
 from .RecurrentSchedule import RecurrentSchedule
 from app.config.database import Base
