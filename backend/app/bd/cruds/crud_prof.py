2a7309de8b7d711e15f859d4cce82a70e93214bd
EstuViani <eviani94@hotmail.com>
2025-04-21 21:13:31 -0300

    Cambio de models. a Profesional

--- backend/app/bd/cruds/crud_prof.py
+++ backend/app/bd/cruds/crud_prof.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import Session
 from app.bd.schemas import schema_prof
-import app.models as models
+from app.models.Profesional import Profesional
 
 
 def get_prof(db: Session):
-    return db.query(models.Profesional).all()
+    return db.query(Profesional).all()
 
 def create_prof(db: Session, prof_c: schema_prof.ProfesionalCreate):
     try:
-        db_prof = models.Profesional(**prof_c.dict())
+        db_prof = Profesional(**prof_c.dict())
         db.add(db_prof)
         db.commit()
         db.refresh(db_prof)
@@ -18,7 +18,7 @@ def create_prof(db: Session, prof_c: schema_prof.ProfesionalCreate):
 
 def get_id_prof(db:Session, id_prof: int):
     try:
-        response = db.query(models.Profesional).get(id_prof)
+        response = db.query(Profesional).get(id_prof)
         if response is None:
             response = {"id":-1, "score":-1}
     except:
