8ae1a0a7ac2c2e01312b1358610e5a6dae62b9ad
EstuViani <eviani94@hotmail.com>
2025-04-23 21:36:19 -0300

    add error handling, and get specific day shedule

--- backend/app/routes/agenda.py
+++ backend/app/routes/agenda.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.config.database import get_db
-from typing import List
+from typing import List, Union
+from pydantic import BaseModel
 from datetime import date, time
 
 from app.bd.schemas.schema_specific import Specific, SpecificCreate, SpecificIsCancel
 from app.bd.schemas.schema_recurrent import Recurrent, RecurrentCreate
 import app.models as models
 from app.bd.cruds import crud_specific, crud_recurrent
-
+from app.bd.schemas.schema_prof_topic import ProfesionalTopic
+from app.bd.schemas.schema_topic import Topic
+from app.bd.bd_utils import Errors
 
 router = APIRouter(prefix="/prof/{prof_id}/agenda", tags=["Agenda"])
 
@@ -21,7 +24,7 @@ def get_all_dias(db: Session = Depends(get_db)):
 
 #Crea un dia especifico para un profesional
 @router.post("/create/specificday", 
-             response_model=Specific, 
+             response_model=Union[Specific, Errors], 
              tags=["Specific"])
 def create_dia(dia: SpecificCreate,
                 prof_id: int, 
@@ -30,7 +33,8 @@ def create_dia(dia: SpecificCreate,
 
 #Consulta el estado del dia y hora, si esta o no cancelado
 @router.get("/specif/{day}/state",
-            response_model=List[SpecificIsCancel],tags=["Specific"])
+            response_model=List[SpecificIsCancel],
+            tags=["Specific"])
 def isCaceled( prof_id:int, day:date, hour:time, db: Session = Depends(get_db)):
     return crud_specific.iscaceled_specific(db, day, prof_id, hour)
 
@@ -55,7 +59,13 @@ def get_dias(prof_id:int, db: Session = Depends(get_db)):
     return crud_recurrent.get_all_recurrent(db, prof_id)
 
 @router.post("/create/recurent", 
-             response_model=Recurrent, 
+             response_model=Union[Recurrent, Errors], 
              tags=["Recurrent"])
 def create_recurrent(dia: RecurrentCreate, prof_id: int, db: Session = Depends(get_db)):
     return crud_recurrent.create_recurrent(db, dia, prof_id)
+
+@router.get("/day/{name_day}",
+            response_model=Union[List[Recurrent],Errors],
+            tags=["Recurrent"])
+def get_day(prof_id:int, name_day:str, db: Session = Depends(get_db)):
+    return crud_recurrent.get_day(db, prof_id, name_day)
