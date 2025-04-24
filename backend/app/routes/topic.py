d097d9052794eef1b81afd8355d2e1612aa08e63
EstuViani <eviani94@hotmail.com>
2025-04-24 20:26:17 -0300

    Add error response

--- backend/app/routes/topic.py
+++ backend/app/routes/topic.py
@@ -1,35 +1,42 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.config.database import get_db
-from typing import List
+from typing import List, Union
 
 from app.bd.schemas.schema_topic import TopicCreate, Topic
 from app.bd.cruds import crud_topic, crud_prof_topic
 from app.bd.schemas.schema_prof_topic import ProfesionalTopicCreate, ProfesionalTopic
-
+from app.bd.bd_utils import Errors
 
 
 router = APIRouter(prefix="/topics",tags=["Topics"])
 
 
-@router.post("/create", response_model=Topic)
+@router.post("/create", 
+             response_model=Topic)
 async def create_topic( topic:TopicCreate, db:Session = Depends(get_db)):
     return crud_topic.create_topic(db, topic)
 
-@router.get("/get",response_model=List[Topic])
+@router.get("/get",
+            response_model=List[Topic])
 async def get_topic(db:Session = Depends(get_db)):
     return crud_topic.get_all_topic(db)
 
-@router.get("/get/{topic_name}", response_model=List[Topic])
+@router.get("/get/{topic_name}", 
+            response_model=List[Topic])
 async def get_one_topic(topic_name:str, db: Session = Depends(get_db)):
     return crud_topic.get_topic(db, topic_name)
 
 
-@router.post("/{topic_name}/prof/{prof_id}",response_model=ProfesionalTopic, tags=["Prof Topic"])
+@router.post("/{topic_name}/prof/{prof_id}",
+             response_model=Union[ProfesionalTopicCreate, Errors], 
+             tags=["Prof Topic"])
 async def add_topic(topic_name:str, prof_id:int, db: Session = Depends(get_db)):
-    prof_topic = ProfesionalTopic(topic_name=topic_name.upper(), user_id=prof_id)
-    return crud_prof_topic.add_topic(db,prof_topic)
+    prof_topic = ProfesionalTopicCreate(topic_name=topic_name.upper(), user_id=prof_id)
+    return crud_prof_topic.add_topic(db, prof_topic)
 
-@router.get("/prof/{prof_id}",response_model=List[ProfesionalTopic], tags=["Prof Topic"])
+@router.get("/prof/{prof_id}", 
+            response_model=List[ProfesionalTopic], 
+            tags=["Prof Topic"])
 async def get_topics(prof_id:int, db:Session =Depends(get_db)):
     return crud_prof_topic.get_topics(db, prof_id)
