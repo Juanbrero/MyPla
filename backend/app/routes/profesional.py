4c5cd89d7783859ec6dee478b12f775dbb77fc88
EstuViani <eviani94@hotmail.com>
2025-04-23 21:30:06 -0300

    Add Error handling

--- backend/app/routes/profesional.py
+++ backend/app/routes/profesional.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.config.database import get_db
-from typing import List
+from typing import List, Union
 
 from app.bd.schemas.schema_prof import Profesional, ProfesionalCreate
 import app.models as models
 from app.bd.cruds import crud_prof
-
+from app.bd.bd_utils import Errors
 
 router = APIRouter(prefix="/profesional",tags=["Profesional"])
 
 @router.post("/create/{prof_id}", 
-             response_model=Profesional)
+             response_model=Union[Profesional,Errors])
 def create_prof(prof : ProfesionalCreate, 
                 db : Session = Depends(get_db)):
     return crud_prof.create_prof(db, prof)
@@ -24,7 +24,7 @@ def read_all_prof(db : Session = Depends(get_db)):
 
 
 
-@router.get("/{id_prof}", response_model=Profesional)
+@router.get("/{id_prof}", response_model=Union[Profesional,Errors])
 def find_prof(id_prof: int, db: Session = Depends(get_db)):
     response = crud_prof.get_id_prof(db,id_prof)        
     return response
