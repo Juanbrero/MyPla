3bd5516109196ad4b2e6c2d95e5f1ba4dfa6198a
EstuViani <eviani94@hotmail.com>
2025-05-02 15:06:58 -0300

    Agregado de modelo User y endpoint para crearlo,  junto a un enpoint que muestra respuesta a enviar

--- backend/app/models/User.py
+++ backend/app/models/User.py
@@ -1,8 +1,12 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.config.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    user_id: Mapped[str] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+
+    professional: Mapped['Professional'] = relationship(back_populates='user', cascade='all, delete-orphan')
+
--- backend/app/routes/prueba.py
+++ backend/app/routes/prueba.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.models.Event import Event
-from app.models.User import User 
+
 
 from app.config.database import get_db
 #Imports to insert in BD
 from sqlalchemy import insert
+from app.models.User import User 
+
 
 
 router = APIRouter()
@@ -15,6 +16,44 @@ router = APIRouter()
 def read_root():
     return {"message": "¡Hola, FastAPI está funcionando!"}
 
-@router.get("/event")
-def get_events(db: Session = Depends(get_db)):
-    return db.query(Event).all()
+
+###
+@router.post('/create/{user_id}',tags=["USER"])
+def create_user(user_id: str, db: Session = Depends(get_db)):
+    try:
+        user = {'user_id': user_id, 'name':user_id}
+        smt =insert(User).values(user)
+        response = db.execute(smt)
+        db.commit()
+        return user
+    except:
+        return {'error':'en la insercion'}
+
+@router.get('/get/all', tags=["USER"])
+def get_all(db:Session = Depends(get_db)):
+    ic(f'GET USER')
+    return db.query(User).all()
+
+@router.delete('/delete/{user_id}',tags=["USER"])
+def delete_user(user_id:str, db:Session = Depends(get_db)):
+    try:
+        res = db.get(User, user_id)
+        db.delete(res)
+        db.commit()
+        return {'OK':'OK'}
+    except:
+        return {'error': 'not delete'}
+
+###
+
+#MUESTRA DE SCHEMA RESPONSE
+#En el body esta la respuesta al alumno
+#En la Respuesta esta la respuesta al Professional
+from ..bd.schemas import schema_response
+@router.post("/RESPONSE", 
+             response_model=schema_response.ResponseProfessional, 
+             tags=['TEST'], description=f" Endpoint para ver esquema de respuesta \
+             -Body: esquema para respuesta alumno \
+             -Response: esquema para professional")
+def test(res:schema_response.Response):
+    return None
