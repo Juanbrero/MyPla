300fa2d1b883c5fbb2837debbcf4303e6efea23c
EstuViani <eviani94@hotmail.com>
2025-05-02 15:08:58 -0300

    Del get de un solo topico, y renombrado de profesional

--- backend/app/models/Topic.py
+++ backend/app/models/Topic.py
@@ -1,11 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.config.database import Base
+from datetime import datetime
+from typing import List
 
 class Topic(Base):
     __tablename__ = "topic"
 
     topic_name: Mapped[str] = mapped_column(primary_key=True)
 
-    profesional_topic: Mapped["ProfesionalTopic"] = relationship(back_populates="topic")
-    #topics: Mapped["TopicRecurrent"] = relationship(back_populates="topic")
+    #create: Mapped[datetime] = mapped_column(default=datetime.today(), server_default='CURRENT_TIMESTAMP') #CURRENT_DATE, CURRENT_TIME 
+
+    professional_topic: Mapped[List["ProfessionalTopic"]] = relationship(back_populates="topic", cascade='all, delete-orphan')
