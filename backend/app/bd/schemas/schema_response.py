4ab7506b44a24eb48259b7335aaff1233fbed3d9
EstuViani <eviani94@hotmail.com>
2025-05-02 15:18:26 -0300

    Esquema que se utilizaria para las respuestas a la agenda

---
+++ backend/app/bd/schemas/schema_response.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel
+from typing import Dict
+from . import schema_specific, schema_topic_recurrent, schema_topic_specific
+
+
+class ResponseRecurrent(BaseModel):
+    recurrent: list[schema_topic_recurrent.TopicRecurrentCr1]
+
+class ResponseSpecific(BaseModel):
+     specific: list[schema_topic_specific.TopicSpecificCr1]
+
+class ResponseException(BaseModel):
+    exception: list[schema_specific.Specific]
+
+class Response(BaseModel):
+    recurrent: list[schema_topic_recurrent.TopicRecurrentCr1]
+    specific: list[schema_topic_specific.TopicSpecificCr1]
+    exception: list[schema_specific.Specific]
+    event: list[schema_specific.SpecificCreate] #Cambiar a esquema de event
+
+class ResponseProfessional(Response):
+    clase: list[schema_specific.Specific] #Cambiar a esquema de clase
+
+class ResponseAlumno(Response):
+    pass
+    
+
+   
+
+"""recurrent: [{
+  start: Time,
+  end: time,
+  week_day: integer o enum (dia recurrente),
+  topics: string[]
+}]
+specific: [{
+  date: Date
+  start: Time,
+  end: time,
+  topics: string[]
+}]
+exception: [{
+  date: Date
+  start: Time,
+  end: time,
+}]
+class: [{
+  start: Time,
+  date: Date,
+  topic: string
+}]
+event: [{
+  start: Time,
+  end: Time,
+  date: Date,
+  id_evento: string // no hay id pero es pa consultar digamo
+}]
+"""
