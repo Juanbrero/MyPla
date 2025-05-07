2039734b207da7edd0dda75a6a0e5a5bae001af4
EstuViani <eviani94@hotmail.com>
2025-05-07 00:00:04 -0300

    Add imports comunes

--- backend/app/models/model_base.py
+++ backend/app/models/model_base.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint, ForeignKeyConstraint 
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.config.database import Base
+from typing import List
+from datetime import datetime
 
 class MODEL(Base):
     __tablename__ = "model"
