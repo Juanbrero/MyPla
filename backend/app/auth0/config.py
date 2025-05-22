94b2d7fb183c25c4aafca08dd42c18f5a29b1866
matias <matiasherrneder@gmail.com>
2025-05-22 17:22:48 -0300

    actualizo env vars

--- backend/app/auth0/config.py
+++ backend/app/auth0/config.py
@@ -7,8 +7,8 @@ class Settings(BaseSettings):
     auth0_mgmt_client_id: str
     auth0_mgmt_client_secret: str
     client_origin_url: str
-    port: int
-    reload: bool
+    # port: int
+    # reload: bool
 
     @classmethod
     @validator("client_origin_url", "auth0_audience", "auth0_domain", "auth0_mgmt_client_id", "auth0_mgmt_client_secret")
