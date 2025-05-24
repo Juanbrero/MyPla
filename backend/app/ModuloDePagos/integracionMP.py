eb24e983520550eab9d35e82cb9ffbd290ac7b18
matias <matiasherrneder@gmail.com>
2025-05-24 12:11:27 -0300

    test pipeline

--- backend/app/ModuloDePagos/integracionMP.py
+++ backend/app/ModuloDePagos/integracionMP.py
@@ -14,7 +14,7 @@ def getPreference():
             {
                 "title": "Mi producto",
                 "quantity": 1,
-                "unit_price": 1.5,
+                "unit_price": 1.6,
             }
         ]
     }
