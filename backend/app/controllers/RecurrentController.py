4ae52c5b0fc295276b1c0e96037fc31ca27fb785
EstuViani <eviani94@hotmail.com>
2025-05-24 23:47:11 -0300

    Update controller

--- backend/app/controllers/RecurrentController.py
+++ backend/app/controllers/RecurrentController.py
@@ -1,30 +1,48 @@
 from ..bd.repositories.MeetingRepository import MeetingRepository
 from ..bd.repositories.ProfessionalTopicRepository import ProfessionalTopicRepository
-from ..bd.repositories.ReservationRepository import ReservationRepository
-from ..bd.repositories.ClassRepository import ClassRepository
 from ..bd.repositories.RecurrentScheduleRepository import RecurrentScheduleRepository
-from ..bd.repositories.SpecificScheduleRepository import SpecificScheduleRepository
 from sqlalchemy.orm import Session
 from ..services.recurrent_services.CreateRecurrent import CreateRecurrent
+from ..services.recurrent_services.GetRecurrentWeek import GetRecurrentWeek
+from ..services.recurrent_services.DelRecurrent import DelRecurrent
+from ..services.recurrent_services.UpRecurrent import UpRecurrent
 from ..bd.schemas import schema_topic_recurrent
 
 class RecurrentController:
     def __init__(self, db: Session):
-        self.professional_topicR = ProfessionalTopicRepository(db)
-        self.recurrentR = RecurrentScheduleRepository,
-        self.classR = ClassRepository(db)
+        self.db = db
         self.recurrentR = RecurrentScheduleRepository(db)
-        self.specificR = SpecificScheduleRepository(db)
-
+        self.meetingR = MeetingRepository(db)
+        self.professional_topicR = ProfessionalTopicRepository(db)
+        
     
     def createRecurrent(self, recurrentS: schema_topic_recurrent.TopicRecurrentIn):
         return CreateRecurrent.run(
             db = self.db,
+            recurrentS = recurrentS,
             recurrentR = self.recurrentR,
             meetingR = self.meetingR,
-            professional_topicR = self.professional_topicR,
-            classR = self.classR,
-            recurrenS = recurrentS,
-            specificR = self.specificR
-        
+            professional_topicR = self.professional_topicR        
+        )
+    
+    def getRecurrentWeek(self, recurrentS: schema_topic_recurrent.TopicRecurrentWeekGet):
+        return GetRecurrentWeek.run(
+            db = self.db,
+            recurrentS = recurrentS,
+            recurrentR = self.recurrentR
+        )
+    
+    def delRecurrent(self, recurrentS: schema_topic_recurrent.TopicRecurrentSchema):
+        return DelRecurrent.run(
+            db = self.db,
+            recurrentS = recurrentS,
+            recurrentR = self.recurrentR
+        )
+    
+    def updateRecurrent(self, recurrentS: schema_topic_recurrent.TopicRecurrentUpdate):
+        return UpRecurrent.run(
+            db = self.db,
+            recurrentS = recurrentS,
+            recurrentR = self.recurrentR,
+            meetingR = self.meetingR
         )
