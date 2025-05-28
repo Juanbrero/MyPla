059e0f211634b3fe9fe6d75cec8082473cdaba7d
matias <matiasherrneder@gmail.com>
2025-05-28 18:14:41 -0300

    add reset db for testing in deploy

---
+++ backend/app/routes/reset_db.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, HTTPException
+from alembic import command
+from alembic.config import Config
+
+app = FastAPI()
+
+@app.post("/reset_db")
+def reset_db():
+    try:
+        alembic_cfg = Config("alembic.ini")
+        command.downgrade(alembic_cfg, "base")
+        command.upgrade(alembic_cfg, "head")
+        return {"status": "Base de datos reseteada correctamente"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
