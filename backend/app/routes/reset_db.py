3109eb8cb18e82ce444746b3c184f724026cfc39
matias <matiasherrneder@gmail.com>
2025-05-28 18:44:24 -0300

    update reset-db

--- backend/app/routes/reset_db.py
+++ backend/app/routes/reset_db.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
 from alembic import command
 from alembic.config import Config
+import os
 
 router = APIRouter()
 
 @router.post("/api/reset_db")
 def reset_db():
     try:
-        alembic_cfg = Config("alembic.ini")
+        base_dir = os.path.dirname(__file__)
+        alembic_cfg = Config(os.path.join(base_dir, '..', 'alembic.ini'))
         command.downgrade(alembic_cfg, "base")
         command.upgrade(alembic_cfg, "head")
         return {"status": "Base de datos reseteada correctamente"}
