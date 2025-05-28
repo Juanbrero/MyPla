204e8e7c59f6091d277b3b63d7360af83e4432db
EstuViani <eviani94@hotmail.com>
2025-05-28 01:50:14 -0300

    Add avaible

---
+++ backend/app/services/available_services/GetStudentAvailable.py
@@ -0,0 +1,67 @@
+from app.utils.errors import handle_errors, MissingData, ValidationError, NotFound
+from app.models import SpecificSchedule, Class , RecurrentSchedule
+from app.bd.repositories.Repository import Repository
+from sqlalchemy.orm import Session
+from app.bd.schemas import schema_response
+from fastapi.responses import JSONResponse
+from fastapi import status
+
+
+class GetStudentAvailable():
+    @handle_errors
+    def run(
+        db = Session,
+        student_id= str,
+        recurrentR= Repository[RecurrentSchedule],
+        exceptionR= Repository[SpecificSchedule],
+        specificR= Repository[SpecificSchedule],
+        classR= Repository[Class]
+    ):
+        
+        all_specifics = specificR.getAllWithTopics(prof_id, False)
+
+        all_exceptions = exceptionR.getAllWithProfessional(prof_id)
+        
+        all_recurrents = recurrentR.getRecurrentsWithTopics(prof_id)
+
+        data_specific = []
+        for schedule in all_specifics:
+            item = {
+                "prof_id": schedule.prof_id,
+                "day": schedule.day.isoformat(),
+                "start": schedule.start.isoformat(),
+                "end": schedule.end.isoformat(),
+                "topics": [topic.topic_name for topic in schedule.topic_specifics]
+            }
+            data_specific.append(item)
+
+        data_recurrent = []
+        for schedule in all_recurrents:
+            item = {
+                "prof_id": schedule.prof_id,
+                "week_day": schedule.week_day,
+                "start": schedule.start.isoformat(),
+                "end": schedule.end.isoformat(),
+                "topics": [topic.topic_name for topic in schedule.topic_recurrents]
+            }
+            data_recurrent.append(item)
+
+        data_exception = []
+        for schedule in all_exceptions:
+            item = {
+                "prof_id": schedule.prof_id,
+                "day": schedule.day.isoformat(),
+                "start": schedule.start.isoformat(),
+                "end": schedule.end.isoformat()
+            }
+            data_exception.append(item)
+        
+        response = {
+            'specific': data_specific,
+            'recurrent': data_recurrent,
+            'exception': data_exception,
+            'event': []
+        }
+
+        
+        return JSONResponse(status_code= status.HTTP_200_OK, content= response)
