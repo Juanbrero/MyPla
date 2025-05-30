440633c444e0083673ad0e208307fc9d16c09962
ThiagoPuyelli <thiagopuyelli@gmail.com>
2025-05-30 14:42:49 -0300

    cambios revertidos para integrar front

--- backend/app/routes/recurrent.py
+++ backend/app/routes/recurrent.py
@@ -13,16 +13,16 @@ router = APIRouter(prefix="/api/recurrent")
 
 
 @router.post('', tags=['Recurrent'])
-def create_recurrent(recurrent:schema_topic_recurrent.TopicRecurrentCr1, db: Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
+def create_recurrent(prof_id: str, recurrent:schema_topic_recurrent.TopicRecurrentCr1, db: Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
     """
     Creacion de un dia recurrente
     """
-    recurrentS = schema_topic_recurrent.TopicRecurrentIn(**recurrent.dict(), prof_id= user_info["user_id"])
+    recurrentS = schema_topic_recurrent.TopicRecurrentIn(**recurrent.dict(), prof_id= prof_id)
     return RecurrentController(db= db).createRecurrent(recurrentS)
 
 @router.get('', tags=['Recurrent'])
-def get_recurre(db: Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
-    return RecurrentController(db= db).getRecurrentProf(user_info["user_id"])
+def get_recurre(prof_id: str, db: Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
+    return RecurrentController(db= db).getRecurrentProf(prof_id)
 
 """
 @router.get('', tags=['Recurrent'])
@@ -34,12 +34,12 @@ def get_week(prof_id:str, week_day: int, db: Session = Depends(get_db)):
 
 
 @router.delete('', tags=['Recurrent'])
-def del_recurrent(week_day:int, start:time ,db: Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
-    recurrentS = schema_topic_recurrent.TopicRecurrentSchema(prof_id= user_info["user_id"], week_day= week_day, start= start)
+def del_recurrent(prof_id: str, week_day:int, start:time ,db: Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
+    recurrentS = schema_topic_recurrent.TopicRecurrentSchema(prof_id= prof_id, week_day= week_day, start= start)
     return RecurrentController(db= db).delRecurrent(recurrentS)
 
 
 @router.put('', tags=['Recurrent'])
-def update_recurrent(update:schema_topic_recurrent.TopicRecurrentUp, db: Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
-    rescurrentS = schema_topic_recurrent.TopicRecurrentUpdate(**update.dict(), prof_id= user_info["user_id"])
+def update_recurrent(prof_id: str, update:schema_topic_recurrent.TopicRecurrentUp, db: Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
+    rescurrentS = schema_topic_recurrent.TopicRecurrentUpdate(**update.dict(), prof_id= prof_id)
     return RecurrentController(db= db).updateRecurrent(rescurrentS)
