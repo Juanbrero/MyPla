ca7437edb75b7749b014e9c809dc39ad76f8b172
ThiagoPuyelli <thiagopuyelli@gmail.com>
2025-05-31 11:06:45 -0300

    Añadir uso de token en los endpoints

--- backend/app/routes/specific.py
+++ backend/app/routes/specific.py
@@ -11,34 +11,34 @@ from app.bd.bd_utils import Errors, Info
 router = APIRouter(prefix="/api/specific")
 
 @router.post('',tags=["Specific"], response_model=Union[schema_topic_specific.TopicSpecificIn, Errors])
-def create_specific_day(prof_id: str, specific:schema_topic_specific.TopicSpecificCr1, db:Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
+def create_specific_day(specific:schema_topic_specific.TopicSpecificCr1, db:Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
     """
     Creacion de un dia especifico
     """
-    topicS = schema_topic_specific.TopicSpecificIn(**specific.dict(), prof_id= prof_id, isCanceling= False)
+    topicS = schema_topic_specific.TopicSpecificIn(**specific.dict(), prof_id= user_info["user_id"], isCanceling= False)
     return SpecificController(db=db).createSpecific(topicS)
 
 @router.get('',tags=["Specific"],response_model=Union[schema_response.ResponseSpecific, List[schema_topic_specific.TopicSpecificCr1], Errors])
-def get_specific(prof_id: str, db:Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
+def get_specific(db:Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
     """
     Recupercion de todos los dias especificos de un profesional
     """
-    return SpecificController(db=db).getAllSpecifics(prof_id)
+    return SpecificController(db=db).getAllSpecifics(user_info["user_id"])
 
 @router.put('',tags=["Specific"],response_model=Union[schema_topic_specific.TopicSpecificUpdate, Errors])
-def update_specific(prof_id: str, specific:schema_topic_specific.TopicSpecificDay,  db:Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
+def update_specific(specific:schema_topic_specific.TopicSpecificDay,  db:Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
     """
     Actualizar dia especifico de un profesional
     """
     
-    topicS = schema_topic_specific.TopicSpecificUpdate(**specific.dict(), prof_id= prof_id)
+    topicS = schema_topic_specific.TopicSpecificUpdate(**specific.dict(), prof_id= user_info["user_id"])
     return SpecificController(db=db).updateSpecific(topicS)
 
 @router.delete('',tags=["Specific"],response_model=Union[schema_topic_specific.TopicSpecificDeleteIn, Errors])
-def delete_specific(prof_id: str, specific:schema_topic_specific.TopicSpecificDeleteCtrl,  db:Session = Depends(get_db)):#, user_info = Depends(RolesValidator(["Profesional"]))):
+def delete_specific(specific:schema_topic_specific.TopicSpecificDeleteCtrl,  db:Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
     """
     Actualizar dia especifico de un profesional
     """
     
-    topicS = schema_topic_specific.TopicSpecificDeleteIn(**specific.dict(), prof_id= prof_id)
+    topicS = schema_topic_specific.TopicSpecificDeleteIn(**specific.dict(), prof_id= user_info["user_id"])
     return SpecificController(db=db).deleteSpecific(topicS)
