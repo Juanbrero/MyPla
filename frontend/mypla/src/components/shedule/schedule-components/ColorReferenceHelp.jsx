77a1fb5c83b76265e0ee2d6a85f9ae5b2f56299e
Juan Brero <juaan.b17@hotmail.com>
2025-06-07 12:24:46 -0300

    correccion menor

--- frontend/mypla/src/components/shedule/schedule-components/ColorReferenceHelp.jsx
+++ frontend/mypla/src/components/shedule/schedule-components/ColorReferenceHelp.jsx
@@ -7,7 +7,7 @@ export const ColorReferenceHelp = () => {
           <ul className="color-list">
             <li className="green" data-tooltip="Horario que se repite solo una vez en una fecha específica">Horario especifico</li>
             <li className="blue" data-tooltip="Horario que se repite regularmente (ej: todos los lunes)">Horario recurrente</li>
-            <li className="orange" data-tooltip="Horario creado para cancelar una franja horaria de un horario recurrente, en una fecha en particular.">Horario excepcion</li>
+            <li className="orange" data-tooltip="Horario creado para cancelar una franja horaria de un horario recurrente, en una fecha en particular.">Excepcion</li>
           </ul>
         </div>
     )
