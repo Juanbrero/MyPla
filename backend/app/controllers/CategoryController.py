50a37258018a99445cdec377732d6002bd911302
EstuViani <eviani94@hotmail.com>
2025-06-18 14:02:02 -0300

    Create Category respository and cotroller

---
+++ backend/app/controllers/CategoryController.py
@@ -0,0 +1,34 @@
+from ..bd.repositories.CategoryRepository import CategoryRepository
+from ..services.category_services.CreateCategory import CreateCategory
+from ..services.category_services.GetCategory import GetCategory
+from ..services.category_services.DelCategory import DelCategory
+
+from sqlalchemy.orm import Session
+
+
+class CategoryController:
+
+    def __init__(self, db:Session):
+        self.db = db
+        self.categoryR = CategoryRepository(db)
+
+    def createCategory(self, category_name:str):
+        return CreateCategory.run(
+            db= self.db,
+            category_name = category_name,
+            categoryR = self.categoryR
+        )
+    
+    def getCategory(self):
+        return GetCategory.run(
+            db = self.db,
+            categoryR= self.categoryR
+        )
+    
+
+    def delCategory(self, category_name:str):
+        return DelCategory.run(
+            db= self.db,
+            category_name = category_name,
+            categoryR = self.categoryR
+        )
