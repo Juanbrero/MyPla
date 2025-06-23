9874a75466ee52fe3835809e29c11650f4b0b0ef
EstuViani <eviani94@hotmail.com>
2025-06-22 23:18:58 -0300

    Add schemas to recibe data in patch and response model to get

--- backend/app/routes/calificate.py
+++ backend/app/routes/calificate.py
@@ -4,14 +4,18 @@ from app.controllers.CalificationController import CalificationController
 from sqlalchemy.orm import Session
 from datetime import datetime
 from app.auth0.dependencies import RolesValidator
+from app.bd.schemas import schema_calificate
 
 router = APIRouter(prefix='/api/calificate', tags=['Calificate'])
 
-@router.get('')
+@router.get('', response_model=schema_calificate.GetCalification)
 def get_class_calificate(db: Session = Depends(get_db), user_info = Depends(RolesValidator(['Alumno', 'Profesional'])) ):
+    """
+        Recibe TOKEN
+    """
     return CalificationController(db= db).getCalificate(user_info['user_id'], user_info['roles'][0])
 
 
 @router.patch('/student')
-def calificate_professional(day_hour: datetime, prof_id: str, score: int, db: Session= Depends(get_db), user_info = Depends(RolesValidator(['Alumno']))):
-    return CalificationController(db= db).calificateProfessional(user_info['user_id'], day_hour, prof_id, score)
+def calificate_professional(calificateS: schema_calificate.Calificate, db: Session= Depends(get_db), user_info = Depends(RolesValidator(['Alumno']))):
+    return CalificationController(db= db).calificateProfessional(user_info['user_id'], calificateS)
