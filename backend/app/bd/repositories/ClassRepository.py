92fcf816efde7f4d4324e612a8d390b85e070a18
EstuViani <eviani94@hotmail.com>
2025-06-22 23:05:07 -0300

    Agregado de querys, para recuperar clases no calificadas por el profesional; recuperar clases calificadas y sumatoria de las calificaciones; validador de clase del estudiante

--- backend/app/bd/repositories/ClassRepository.py
+++ backend/app/bd/repositories/ClassRepository.py
@@ -1,8 +1,8 @@
 from app.models import Class, Reservation, Meeting, Professional
 from sqlalchemy.orm import Session
 from .Repository import Repository
-from sqlalchemy import select, and_
-
+from sqlalchemy import select, and_, func, or_
+from datetime import datetime
 
 class ClassRepository(Repository[Class]):
     def __init__(self, session: Session):
@@ -28,3 +28,85 @@ class ClassRepository(Repository[Class]):
                    Reservation.state == "pay")
         )
         return self.session.execute(smt)
+    
+    def getCalificateProfessional(self, student_id:str):
+        """
+            Consulta que busca todas las clases PAGADAS o FINALIZADAS, que ya superaron su inicio y que aun no fueron calificadas
+            - Recibe un student_id para buscar sus clases
+
+        """
+        stm = (
+            select(Class)
+            .join(Reservation, and_(
+                Reservation.day_hour == Class.day_hour,
+                Reservation.prof_id == Class.prof_id))
+            .where(
+                or_(
+                    Reservation.state == 'pay', 
+                    Reservation.state == 'finish'
+                ),
+                Reservation.student_id == student_id,
+                func.now() >= Class.day_hour,
+                Class.calificate_teacher.is_(None))
+            )
+        return self.session.execute(stm).scalars().all()
+    
+    def getCalificateStudent(self, prof_id:str):
+        """
+        Consulta que busca todas las clases PAGADAS o FINALIZADAS, que ya superaron su inicio y que aun no fueron calificadas
+            - Recibe un pro_id para buscar sus clases
+        """
+        stm = (
+            select(Class)
+            .join(Reservation, and_(
+                Reservation.day_hour == Class.day_hour,
+                Reservation.prof_id == Class.prof_id
+            ))
+            .where(Class.calificate_student.is_(None),
+                   func.now() >= Class.day_hour,
+                   Class.prof_id == prof_id,
+                   or_(
+                    Reservation.state == 'pay', 
+                    Reservation.state == 'finish'
+                ))
+        )
+        return self.session.execute(stm).scalars().all()
+
+    def getCalificationProfessional(self, prof_id: str):
+        """
+         Recupera las calificaciones de un profesional
+             Args
+                - prof_id
+             Return
+                - Calification( count, sum)
+        
+        """
+        stm = (
+            select(func.count(Class.calificate_teacher), func.sum(Class.calificate_teacher))
+            .where(
+                func.now() >= Class.day_hour,
+                Class.prof_id == prof_id,
+                Class.calificate_teacher.is_not(None)
+            )
+        )
+        return self.session.execute(stm).first()
+    
+    def getClass(self, prof_id: str, day_hour:datetime, student_id:str):
+        stm = (
+            select(Class)
+            .join(Reservation, and_(
+                Reservation.student_id == student_id,
+                Reservation.day_hour == day_hour,
+                Reservation.prof_id == prof_id
+            ))
+            .where(
+                or_(
+                    Reservation.state == 'pay', 
+                    Reservation.state == 'finish'
+                ),
+                Reservation.prof_id == prof_id,
+                Reservation.day_hour == day_hour,
+                Class.calificate_teacher.is_(None)
+            )
+        )
+        return self.session.execute(stm).first()
