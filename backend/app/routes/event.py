53a2e83561c51c36c6656c9a56d6a8d6be2f407c
ThiagoPuyelli <thiagopuyelli@gmail.com>
2025-06-23 13:23:48 -0300

    Uso de id de autenticacion en los eventos

--- backend/app/routes/event.py
+++ backend/app/routes/event.py
@@ -8,9 +8,9 @@ from app.auth0.dependencies import RolesValidator
 router = APIRouter(prefix='/api/event')
 
 @router.post('', tags=['Event'], response_model= schema_event.EventBase )
-def create_event(eventS: schema_event.EventBase, db:Session = Depends(get_db)): #user_info = Depends(RolesValidator(["Profesional"]))):
+def create_event(eventS: schema_event.EventBase, db:Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
 
-    return EventController(db= db).createEvent(prof_id = "a3f6afdf-0239-454e-a184-efdaf41fcc86", eventS = eventS)
+    return EventController(db= db).createEvent(prof_id = user_info["user_id"], eventS = eventS)
 
 
 @router.get('', tags=['Event'], response_model= schema_event.EventGet )
@@ -19,6 +19,6 @@ def get_events(eventS: schema_event.EventGet = Depends(), db:Session = Depends(g
     return EventController(db= db).getEvents(eventS = eventS)
 
 @router.patch('/invite', tags=['Event'], response_model= schema_event.InviteConfirm)
-def accept_event(inviteS: schema_event.InviteConfirm, db:Session = Depends(get_db)): #user_info = Depends(RolesValidator(["Profesional"]))):
+def accept_event(inviteS: schema_event.InviteConfirm, db:Session = Depends(get_db), user_info = Depends(RolesValidator(["Profesional"]))):
 
-    return EventController(db= db).acceptInvite(invite_id = "9bb43560-9d90-403d-933d-bc3ac6d6abbc", inviteS = inviteS)
+    return EventController(db= db).acceptInvite(invite_id = user_info["user_id"], inviteS = inviteS)
